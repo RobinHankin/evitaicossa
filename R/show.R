1ffc3e5624efae4796f5600cced58b4eb0436d2e
robin <hankin.robin@gmail.com>
2024-05-14 21:58:54 +0100

    slightly better show (still not very good)

--- R/show.R
+++ R/show.R
@@ -9,8 +9,26 @@ aaa_show <- function(a){
     sep=" ")
 }
 
-single_string <- function(a){paste(paste(sc(a),s1(a),sep=""),collapse=" + ")}
-double_string <- function(a){paste(paste(sc(a),d1(a),d2(a),sep=""),collapse=" + ")}
-triple_string <- function(a){paste(paste(sc(a),"(",t1(a),t2(a),")",t3(a),sep=""),collapse=" + ")}
-    
+single_string <- function(a){
+    if(length(sc(a))>0){
+        paste(paste(sc(a),s1(a),sep=""),collapse=" + ")
+    } else {
+        ""
+    }
+}
 
+double_string <- function(a){
+    if(length(dc(a))>0){
+        paste(paste(sc(a),d1(a),d2(a),sep=""),collapse=" + ")
+    } else {
+        ""
+    }
+}
+
+triple_string <- function(a){
+    if(length(tc(a))>0){
+        paste(paste(tc(a),"(",t1(a),t2(a),")",t3(a),sep=""),collapse=" + ")
+    } else {
+        ""
+    }
+}
