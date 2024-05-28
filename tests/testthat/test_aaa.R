e37bf7c25ab11dbbe91878fbdbae90edcb4f7740
robin <hankin.robin@gmail.com>
2024-05-28 17:38:43 +0100

    add a test suite

---
+++ tests/testthat/test_aaa.R
@@ -0,0 +1,103 @@
+## This file follows the structure of aaa.R in the free group package.
+
+## Define some checker functions, and call them at the end.  They
+## should all return TRUE if the package works, and stop with error if
+## a test is failed.  Function checker1() has one argument, checker2()
+## two, and so on.
+
+test_that("Test suite aaa.R",{
+
+checker1 <- function(x){
+
+    expect_error(x <  x)
+    expect_error(x >  x)
+    expect_error(x <= x)
+    expect_error(x >= x)
+
+    expect_error(x == 0)
+    expect_error(x == 3)
+    expect_error(x/x)
+    expect_error(x^x)
+    expect_error(x^(-1))
+  
+    expect_error(x+1)
+    expect_error(x-1)
+    expect_error(1+x)
+    expect_error(1-x)
+
+    expect_true(x == x, info=x)
+    expect_true(x == +x, info=x)
+    expect_true(+x == x, info=x)
+    expect_true(x == -(-x), info=x)
+  
+    expect_true(x+x == 2*x, info=x)
+    expect_true(x+x == x*2, info=x)
+    expect_true(x+x-x == x, info=x)
+
+    expect_true(is.zero(x-x), info=x)
+    expect_true(is.zero(x*0), info=x)
+
+    expect_true(x/2 + x/2 == x, info=x)
+  
+    expect_error(x^0, info=x)
+    expect_true(x^1 == x, info=x)
+    expect_true(x^2 == x*x, info=x)
+    expect_error(x^3, info=x)
+    expect_true(is.zero(x^4), info=x)
+    expect_true(is.zero(x^5), info=x)
+    expect_true(is.zero(x^6), info=x)
+
+    expect_output(print(x))
+
+    return(TRUE)
+}  # checker1() closes
+
+
+checker2 <- function(x,y){
+  expect_true(x == -y+x+y, info=list(x,y))
+  expect_true(x+y == x-(-y), info=list(x,y))
+
+  expect_true(x+y == y+x, info=list(x,y))
+
+  expect_true((-x)*y == -(x*y), info=list(x,y))
+  expect_true(x*(-y) == -(x*y), info=list(x,y))
+
+  return(TRUE)
+}
+
+
+checker3 <- function(x,y,z){
+  expect_true(x+(y+z) == (x+y)+z, info=list(x,y,z)) # additive associativity
+  expect_true(x*(y*z) == -(x*y)*z, info=list(x,y,z)) # The sine qua non of the whole package!
+
+  expect_true(x*(y+z) == x*y + x*z, info=list(x,y,z))  # left distributivity
+  expect_true((y+z)*x == y*x + z*x, info=list(x,y,z))  # right distributivity
+
+  a <- y
+  b <- z
+  expect_true((a+a*x)*(b+x*b) == a*b)
+
+  return(TRUE)
+} # checker3() closes
+
+checker4 <- function(x,y,z,p){
+    expect_true(is.zero(x*y*z*p))
+}
+
+
+for(i in 1:2){
+    x <- raaa()
+    y <- raaa()
+    z <- raaa()
+    p <- raaa()
+    
+    checker1(x)
+    checker2(x,y)
+    checker3(x,y,z)
+    checker4(x,y,z,p)
+}
+
+checker1(aaa())
+
+
+})
