8813621a5ce708ae1fc3b05005cc632404973cfc
robin <hankin.robin@gmail.com>
2024-05-28 20:56:36 +0100

    linear functions

---
+++ tests/testthat/test_aab.R
@@ -0,0 +1,16 @@
+test_that("Test suite aab.R",{
+
+checker <- function(a,b,x,y){
+
+    expect_true(linear1(a*x + b*y) == a*linear1(x) + b*linear1(y))
+    expect_true(linear2(a*x + b*y) == a*linear2(x) + b*linear2(y))
+    expect_true(linear3(a*x + b*y) == a*linear3(x) + b*linear3(y))
+
+    return(TRUE)
+}  # checker1() closes
+
+for(i in 1:2){
+    checker(sample(9,1),sample(9,1),sample(9),sample(9))
+}
+
+})
